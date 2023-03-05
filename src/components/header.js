cd60ae85577776500f2135fe5e328bdfec14d7ef
Karthik99999 <bandagondak0217@gmail.com>
2023-03-04 22:24:23 -0500

    Set up router

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">LanguageLearner</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+
+function Home() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Home;
